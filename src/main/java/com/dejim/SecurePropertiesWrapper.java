228c840068b278fc161a7e1a65a89e0ffb3e65b9
Dejim Juang <dejimj@gmail.com>
2020-05-09 20:39:29 -0500

    File and File Level
    Added File and File Level encryption. Also changed to HTTPS

--- src/main/java/com/dejim/SecurePropertiesWrapper.java
+++ src/main/java/com/dejim/SecurePropertiesWrapper.java
@@ -32,4 +32,54 @@ public class SecurePropertiesWrapper {
 		}
 		return response.toString();
 	}
+	
+	public String secureFile(String appHome, String operation, String algorithm, String mode, String key, String inputFileLocation, String outputFile) {
+		StringBuffer response = new StringBuffer();
+		String line;
+		Process process;
+
+		String toolString = "java -jar " + appHome + "/secure-properties-tool.jar file " + operation + " " + algorithm + " " + mode + " " + key + " " + inputFileLocation + " " + appHome + "/" + outputFile;
+		try {
+
+			process = Runtime.getRuntime().exec(String.format(toolString));
+			process.waitFor();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			while ((line = reader.readLine()) != null) {
+				response.append(line);
+			}
+			while ((line = error.readLine()) != null) {
+				response.append(line);
+			}
+		} catch (IOException e1) {
+		} catch (InterruptedException e2) {
+		}
+		return response.toString();
+	}
+	
+	public String secureFileLevel(String appHome, String operation, String algorithm, String mode, String key, String inputFileLocation, String outputFile) {
+		StringBuffer response = new StringBuffer();
+		String line;
+		Process process;
+
+		String toolString = "java -jar " + appHome + "/secure-properties-tool.jar file-level " + operation + " " + algorithm + " " + mode + " " + key + " " + inputFileLocation + " " + appHome + "/" + outputFile;
+		try {
+
+			process = Runtime.getRuntime().exec(String.format(toolString));
+			process.waitFor();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			while ((line = reader.readLine()) != null) {
+				response.append(line);
+			}
+			while ((line = error.readLine()) != null) {
+				response.append(line);
+			}
+		} catch (IOException e1) {
+		} catch (InterruptedException e2) {
+		}
+		return response.toString();
+	}
 }
